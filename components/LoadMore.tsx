b50ad7c612835f22b9b51ecbf16af6db07efad71
Didier Peran Ganthier <didierganthierperan@gmail.com>
2024-02-08 03:31:19 -0500

    Track view changes in useEffect

--- components/LoadMore.tsx
+++ components/LoadMore.tsx
@@ -1,15 +1,27 @@
 "use client";
 
+import { fetchAnime } from "@/app/action";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 import { useInView } from "react-intersection-observer";
+import { AnimeProp } from "./AnimeCard";
 
 function LoadMore() {
   const { ref, inView } = useInView();
+  const [data, setData] = useState<AnimeProp[]>([]);
+
+  useEffect(() => {
+    if (inView) {
+      fetchAnime(2).then((res) => {
+        setData([...data, ...res]);
+      });
+    }
+  }, [inView, data]);
 
   return (
     <>
       <section className="flex justify-center items-center w-full">
-        <div>
+        <div ref={ref}>
           <Image
             src="./spinner.svg"
             alt="spinner"
