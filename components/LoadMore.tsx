e05c6c28b2e5524b12cc8034d0427cec5eb8e389
Didier Peran Ganthier <didierganthierperan@gmail.com>
2024-02-08 03:37:42 -0500

    Load more animes

--- components/LoadMore.tsx
+++ components/LoadMore.tsx
@@ -4,7 +4,7 @@ import { fetchAnime } from "@/app/action";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import { useInView } from "react-intersection-observer";
-import { AnimeProp } from "./AnimeCard";
+import AnimeCard, { AnimeProp } from "./AnimeCard";
 
 function LoadMore() {
   const { ref, inView } = useInView();
@@ -20,6 +20,11 @@ function LoadMore() {
 
   return (
     <>
+      <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
+        {data.map((item: AnimeProp, index: number) => (
+          <AnimeCard key={item.id} anime={item} index={index} />
+        ))}
+      </section>
       <section className="flex justify-center items-center w-full">
         <div ref={ref}>
           <Image
